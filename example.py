e0f887fcf84d5f46063b3817f03f96b581cc3b18
belzner <fabian.belzner@baw.de>
2025-05-28 15:29:15 +0200

    translate

--- example.py
+++ example.py
@@ -24,11 +24,10 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #%% Module laden
 
-from engineer import labyrinth, betriebsmodell, klappe, optimale_Labyrinth, UW_interpolation,kopplung,tosbecken,plot_check_FAA_FAbA, write_flap_excel, write_lab_excel
 import matplotlib.pyplot as plt
 import numpy as np
-import os, shutil
-import pandas as pd
+
+from engineer import labyrinth, operational_model, klappe, optimale_Labyrinth, write_flap_excel, write_lab_excel
 
 plt.close('all')
 
@@ -36,34 +35,34 @@ plt.close('all')
 # Initialisierung
 # Initialise the labyrinth-weir object
 
-lab = labyrinth(bottom_level=0.1,  #bottom height [m]
-                downstream_water_level=1.09,  #downstream Water level [m]
-                discharge=10,  #discharge [m3/s]
-                labyrinth_width=15,  #labyrinth weir width [m]
-                labyrinth_height=2.2,  #labyrinth weir height [m]
-                labyrinth_length=8,  #labyrinth weir length in flow direction [m]
-                labyrinth_key_angle=8,  #key angle [degree]
-                D=0.5)                  #front wall width [m]
+labyrinth_weir = labyrinth(bottom_level=0.1,  #bottom height [m]
+                           downstream_water_level=1.09,  #downstream Water level [m]
+                           discharge=10,  #discharge [m3/s]
+                           labyrinth_width=15,  #labyrinth weir width [m]
+                           labyrinth_height=2.2,  #labyrinth weir height [m]
+                           labyrinth_length=8,  #labyrinth weir length in flow direction [m]
+                           labyrinth_key_angle=8,  #key angle [degree]
+                           D=0.5)                  #front wall width [m]
 
 # Adjust parameters
-# After adjustment of parameters, the hydraulics ans geometry mus be recalculated
+# After adjustment of parameters, the hydraulics and geometry mus be recalculated
 # lab.D = 0.5
 # lab.geometrie()
 # lab.update()
 
-# Plot the labyerinth geometyr
+# Plot the labyrinth geometry
 # lab.plot_geometry()
 
 
 #%% flap gate
 # (Fischbauch)klappe
 # Usage like the labyrinth
-kla  = klappe(sohleHoehe=0.1,       #bottom height [m]
-              UW=1.09,              #downstream water level [m]
-              Q=10,                 #discharge [m3/s]
-              klappeBreite=1.4,     #flap width [m]
-              klappeHoehe=2.35,     #flap height [m]
-              klappeWinkel=74)      #flap angle [degree]
+flap_gate  = klappe(bottom_level=0.1,  #bottom height [m]
+                    downstream_water_level=1.09,  #downstream water level [m]
+                    discharge=10,  #discharge [m3/s]
+                    flap_gate_width=1.4,  #flap width [m]
+                    flap_gate_height=2.35,  #flap height [m]
+                    flap_gate_angle=74)      #flap angle [degree]
 
 
 
@@ -73,36 +72,30 @@ kla  = klappe(sohleHoehe=0.1,       #bottom height [m]
 # - the design discharge
 # - the crest height of the labyrinth (= minimum upstream water level)
 
-sohlhoehe = 0.1         #bottom height [m]
-breiteBaufeld = 10      #available width for the labyrinth weir [m]
-laengeBaufeld = 8       #available length in flow direction for the labyrinth weir [m]
-Q_HQ = 20               #design discharge [m3/s]
-UW_HQ = 1.8             #downstream water level at design discharge [m]
-OK_Labyrinth = 2.2      #crest height of labyrinth weir [m]    
+bottom_level = 0.1         #bottom height [m]
+labyrinth_width = 10      #available width for the labyrinth weir [m]
+labyrinth_length = 8       #available length in flow direction for the labyrinth weir [m]
+design_discharge = 20               #design discharge [m3/s]
+design_downstream_water_level = 1.8             #downstream water level at design discharge [m]
+labyrinth_crest_height = 2.2      #crest height of labyrinth weir [m]
 
-# Optimization: the return value is an labyrinth-object
-bestLab = optimale_Labyrinth(labyrinth, sohlhoehe, UW_HQ, Q_HQ, breiteBaufeld, OK_Labyrinth-sohlhoehe+0, laengeBaufeld,path='',show_plot=False)
+# Optimization: the return value is a labyrinth-object
+optimized_labyrinth = optimale_Labyrinth(labyrinth, bottom_level, design_downstream_water_level, design_discharge, labyrinth_width, labyrinth_crest_height - bottom_level + 0, labyrinth_length, path='', show_plot=False)
 
 # Postprozess
-bestLab.plot_geometry()     #plot the optimized geometry
-bestLab.verbose = 1         #print output
-bestLab.print_results()     #print result parameters
-
-
-
+optimized_labyrinth.plot_geometry()     #plot the optimized geometry
+optimized_labyrinth.verbose = 1         #print output
+optimized_labyrinth.print_results()     #print result parameters
 
 #%% Operational model
 # Here, the effect of the labyrinth weir system (labyrinth+flap) is simulated along the entire discharge curve.
 # In doing so, the target water level is maintained for as long as possible by controlling the flap.
-Stauziel = 2.2          #design water level [m]
-Kalpha_max = 90         #maximum opening angle of the flap gate [degree]
-H_fische = 0.4          #body height of the design fish [m]
-
-
+design_upstream_water_level = 2.2          #design water level [m]
+max_flap_gate_angle = 90         #maximum opening angle of the flap gate [degree]
+fish_body_height = 0.4          #body height of the design fish [m]
 
 # Hydrology
 # Is required to calculate the hydraulic effect along the entire discharge curve
-
 discharge = np.array([
         2.09,
         2.79,
@@ -114,7 +107,7 @@ discharge = np.array([
         18.60,
         20.50,
         22.90,
-        24.50]) 
+        24.50])
 
 downstream_water_level = np.array([
             1.07,
@@ -146,25 +139,25 @@ upstream_water_level_today = np.array([
 
 #UW_interpolation(Abfluss,Unterwasser,interpolation='all',show_plot=True, save_plot=True)
 
-results, results_events = betriebsmodell(Lab = bestLab,
-                              Kla =  kla,
-                              Abfluss = discharge,
-                              Unterwasser = downstream_water_level,
-                              Oberwasser = upstream_water_level_today,
-                              Stauziel = Stauziel,
-                              KlappeWinkel_max = Kalpha_max,
-                              H_fische= H_fische,
-                              interpolation='exponential',
-                              show_plot=False,
-                              save_plot=False)
+results, results_events = operational_model(labyrinth_object = optimized_labyrinth,
+                                            flap_gate_opject = flap_gate,
+                                            discharge_vector= discharge,
+                                            downstream_water_level_vector = downstream_water_level,
+                                            upstream_water_level_vector = upstream_water_level_today,
+                                            design_upstream_water_level = design_upstream_water_level,
+                                            max_flap_gate_angle= max_flap_gate_angle,
+                                            fish_body_height= fish_body_height,
+                                            interpolation_method='exponential',
+                                            show_plot=False,
+                                            save_plot=False)
 
 
 
 
 
-        
-    
-write_lab_excel(lab)
-write_flap_excel(kla)
+
+
+write_lab_excel(labyrinth_weir)
+write_flap_excel(flap_gate)
 
 
